2ea9cfdf744153d66c9f68bdab0bfe5b62087b79
clemane <clement.lemane@etudiant.univ-rennes1.fr>
2021-04-18 22:52:12 +0200

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(new MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      title: "Recycl'Rennes",
+      theme: new ThemeData(primarySwatch: Colors.blueGrey),
+      debugShowCheckedModeBanner: false,
+      home: new Home(),
+    );
+  }
+}
+
+class Home extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return new _Home();
+  }
+}
+
+class _Home extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: new AppBar(
+        title: new Text("Recycl'Rennes"),
+        leading: new Icon(Icons.account_box),
+        actions: <Widget>[
+          new Text("Nourriture"),
+          new Text("Electroménager"),
+        ],
+        centerTitle: true,
+        elevation: 20.0,
+        backgroundColor: Colors.black,
+      ),
+      backgroundColor: Colors.white,
+      body: new Container(
+        color: Colors.grey,
+        child: Text("Bouffe"),
+        height: MediaQuery.of(context).size.height / 10,
+        width: MediaQuery.of(context).size.height,
+        margin: EdgeInsets.only(top: 10.0, right: 10.0, left: 10.0),
+      ),
+    );
+  }
+}
