48b4a09866bf76ad195fab8c5f2b294ae2547562
nbasle <nathan.basle@etudiant.univ-rennes1.fr>
2021-05-02 19:30:45 +0200

    Amelioration de la page connexion

--- lib/widgets/connexion.dart
+++ lib/widgets/connexion.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_2/widgets/home.dart';
+import 'profile.dart';
 
 class Connexion extends StatefulWidget {
   @override
@@ -6,38 +8,64 @@ class Connexion extends StatefulWidget {
 }
 
 class _ConnexionState extends State<Connexion> {
+  bool secret = true;
+  final RegExp emailRegex = RegExp(r"[a-z0-9\._-]+@[a-z0-9\._-]+\.[a-z]+");
+  final GlobalKey<FormState> formKey = GlobalKey<FormState>();
+
+  String email;
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
         child: Scaffold(
             body: Center(
+                child: SingleChildScrollView(
       child: Column(
         children: [
-          SizedBox(
-            height: 100,
-          ),
           Text(
-            "Recycl\'Rennes",
+            "Recycl'Rennes",
             style: TextStyle(fontSize: 40),
           ),
           SizedBox(
             height: 30,
           ),
-          TextFormField(
-            decoration: InputDecoration(
-                hintText: 'Entrez votre email', border: OutlineInputBorder()),
+          Form(
+            key: formKey,
+            child: TextFormField(
+              onChanged: (value) => setState(() => email = value),
+              validator: (value) => value.isEmpty || !emailRegex.hasMatch(value)
+                  ? 'Merci de rentrer un email valide'
+                  : null,
+              decoration: InputDecoration(
+                  hintText: 'Entrez votre email', border: OutlineInputBorder()),
+            ),
           ),
           SizedBox(
             height: 20,
           ),
           TextFormField(
+            obscureText: secret,
             decoration: InputDecoration(
+                suffixIcon: InkWell(
+                  onTap: () => setState(() => secret = !secret),
+                  child: Icon(secret ? Icons.visibility : Icons.visibility_off),
+                ),
                 hintText: 'Entrez votre mot de passe',
                 border: OutlineInputBorder()),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              if (formKey.currentState.validate())
+                Navigator.push(context,
+                    new MaterialPageRoute(builder: (BuildContext context) {
+                  return new Home();
+                }));
+            },
+            child: Text('CONTINUER'),
           ),
         ],
         crossAxisAlignment: CrossAxisAlignment.center,
       ),
-    )));
+    ))));
   }
 }
